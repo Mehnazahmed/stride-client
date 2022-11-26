c3ca46354d958e81ff6e2157d3492eb8174c5b2e
mehnazimu <mehnaz.imu@gmail.com>
2022-11-26 07:42:19 +0600

    categories loaded

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categories from './Categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <h2>home</h2>
+            <Categories></Categories>
         </div>
     );
 };
